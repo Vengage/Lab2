47815e1523fcaac47b901c496a9c7aa3e733a3ea
Vengage <vengage_92@yahoo.com>
2014-10-26 16:18:38 +0200

    Adagare proiect Lab2

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Cosovanu Vasile 26/10/2014
+ *
+ * This is a class comment
+ */
+
+
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
